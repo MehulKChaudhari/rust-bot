04d496c5a67bf3af6d60330d021f93b7027779e8
Mehul Kiran Chaudhari <chaudharimehul2001@gmail.com>
2023-10-29 17:19:41 +0530

    add help command

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::env;
+
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+const HELP_MESSAGE: &str = "
+Hello there, **Welcome to Real Dev Squad!**
+
+If you are new here, head to the `#lift-simulation` channel for your first task.
+
+❓ Something wrong?
+➡️ You can flag an admin with @admin
+
+Need assistance or have specific questions? Feel free to ask our friendly community members or moderators. Enjoy your time here at Real Dev Squad!
+
+I hope that resolves your issue! For more information about our bot's commands, type `!help` in any channel.
+
+— HelpBot 🤖
+";
+
+const HELP_COMMAND: &str = "!help";
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == HELP_COMMAND {
+            if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+
+    let mut client = Client::new(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
